8cba66fac3a696b3cff0cc7b7e0fa78263e58765
TaurusCollective <taurus.collective.crypto@gmail.com>
2022-01-31 18:43:28 +0100

    small change

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
-        <title>Instagram 2.0</title>
+        <title>TAURUSnet</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
